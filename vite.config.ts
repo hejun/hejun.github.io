06b9973a314723c9dd45dce5591f77f3d2f41ae6
HeJun <1255524587@qq.com>
2025-05-06 17:59:34 +0800

    版本升级

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '#': path.resolve(__dirname, 'types')
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
